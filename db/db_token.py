816f04d995015673355cf42063c2567614083216
thenishantsapkota <snishant306@gmail.com>
2021-07-30 17:37:00 +0545

    [Remove] Deletes .gitignore

---
+++ db/db_token.py
@@ -0,0 +1,21 @@
+from modules.imports import *
+
+engine = create_engine("sqlite:///db/database.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class BotToken(Base):
+
+    __tablename__ = "bot_key"
+
+    bot_token = Column(String, primary_key=True)
+
+    def __init__(self, bot_token):
+        self.bot_token = bot_token
+
+
+for r in session.query(BotToken):
+    token = r.bot_token
