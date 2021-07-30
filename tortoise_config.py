310a8a74a42fcb2f8e0e86255225bbe064c50dc7
thenishantsapkota <snishant306@gmail.com>
2021-07-30 17:41:55 +0545

    [Remove] Deletes something

--- tortoise_config.py
+++
@@ -1,9 +0,0 @@
-tortoise_config = {
-    "connections": {"default": "sqlite://db/database.db"},
-    "apps": {
-        "app": {
-            "models": ["models", "aerich.models"],
-            "default_connection": "default",
-        },
-    },
-}
