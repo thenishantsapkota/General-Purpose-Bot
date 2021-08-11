d1dd6044a5732776463c521ada1fc27ff22457f4
Nishant Sapkota <snishant306@gmail.com>
2021-08-11 16:32:35 +0545

    [Update] Adds mute handler on when bot crashes and moderation role commands

--- zorander/cogs/utils.py
+++ zorander/cogs/utils.py
@@ -3,11 +3,13 @@ import logging
 from datetime import datetime
 
 import discord
-from discord import Embed
+import pytz
+from discord import Color, Embed, Role
 from discord.ext import commands
 from discord.ext.commands import Cog, command
 
 from zorander import Bot
+from zorander.core.models import MuteModel, ModerationRoles
 
 from ..core.models import GuildModel
 
@@ -36,6 +38,140 @@ class Utils(Cog):
         prefix = model.prefix
         response = f"Hello {ctx.author.name}, My prefix here is {self.bot.user.mention} or `{prefix}`\nUse the `{prefix}changeprefix <new_prefix>` command to change it."
         await ctx.send(response)
+
+    @Cog.listener()
+    async def on_ready(self):
+        await asyncio.sleep(5)
+        models = MuteModel.all()
+        async for model in models:
+            asyncio.create_task(self.mute_handler(model))
+
+    async def mute_handler(self, model: MuteModel):
+        utc = pytz.UTC
+        localized_mutetime = model.time
+        localized_nowtime = utc.localize(datetime.now())
+        if localized_mutetime > localized_nowtime:
+            remaining_time = (localized_mutetime - localized_nowtime).total_seconds()
+            await asyncio.sleep(remaining_time)
+            await self.mute_handler_get(model)
+            # print("Success")
+            return
+        await self.mute_handler_get(model)
+        # print("Sucess - A")
+
+    async def mute_handler_get(self, model: MuteModel):
+        guild = self.client.get_guild(model.guild_id)
+        member = guild.get_member(model.member_id)
+        logChannel = discord.utils.get(guild.text_channels, name="mod-logs")
+        role_ids = model.role_id
+        roles = [guild.get_role(int(id_)) for id_ in role_ids.split(",") if len(id_)]
+        await member.edit(roles=roles)
+        await model.delete()
+        embed = Embed(
+            description=f"**:loud_sound: Unmuted {member} [ID {member.id}]**",
+            color=Color.green(),
+            timestamp=datetime.utcnow(),
+        )
+        embed.set_author(
+            name=f"{self.client.user} [ID {self.client.user.id}]",
+            icon_url=self.client.user.avatar_url,
+        )
+        embed.add_field(name="Reason", value="Mute Duration Expired.")
+        embed.set_thumbnail(url=member.avatar_url)
+        await logChannel.send(embed=embed)
+
+    @commands.group(invoke_without_command=True)
+    async def role(self, ctx):
+        """Role group to set moderation roles for the server."""
+        embed = Embed(
+            color=self.bot.color,
+            timestamp=datetime.utcnow(),
+        )
+        embed.add_field(
+            name=f"**Arguments**",
+            value=f"`admin` - Set admin role for the server.\n\n`mod` - Set mod role for the server\n\n`staff` - Set staff role for the server.\n\n`give` - Give a role to the user.\nExample: `role give <member> <role>`\n\n`take` - Take a role from the user.\nExample: `role take <member> <role>`",
+        )
+        embed.set_author(
+            name=f"Help with role command.", icon_url=self.bot.user.avatar_url
+        )
+        embed.set_footer(text=f"Invoked by {ctx.author}")
+        await ctx.send(embed=embed)
+
+    @role.group(name="admin")
+    @commands.has_permissions(administrator=True)
+    async def adminroleset(self, ctx, role: Role):
+        """Set admin role for the server."""
+        guild = ctx.guild
+        author = ctx.author
+
+        if role in ctx.guild.roles:
+            model, _ = await ModerationRoles.get_or_create(guild_id=guild.id)
+            model.admin_role = role.id
+            await model.save()
+            embed = Embed(
+                title="Admin Role",
+                description=f"The admin role  for {ctx.guild.name} has been set to {role.mention}",
+                timestamp=datetime.utcnow(),
+                color=self.bot.color,
+            )
+            await ctx.send(embed=embed)
+
+    @role.group(name="mod")
+    @commands.has_permissions(administrator=True)
+    async def modroleset(self, ctx, role: Role):
+        """Set mod role for the server."""
+        guild = ctx.guild
+        author = ctx.author
+
+        if role in ctx.guild.roles:
+            model, _ = await ModerationRoles.get_or_create(guild_id=guild.id)
+            model.mod_role = role.id
+            await model.save()
+            embed = Embed(
+                title="Moderator Role",
+                description=f"The mod role  for {ctx.guild.name} has been set to {role.mention}",
+                timestamp=datetime.utcnow(),
+                color=self.bot.color,
+            )
+            await ctx.send(embed=embed)
+
+    @role.group(name="staff")
+    @commands.has_permissions(administrator=True)
+    async def staffroleset(self, ctx, role: Role):
+        """Set staff role in server"""
+        guild = ctx.guild
+        author = ctx.author
+
+        if role in ctx.guild.roles:
+            model, _ = await ModerationRoles.get_or_create(guild_id=guild.id)
+            model.staff_role = role.id
+            await model.save()
+            embed = Embed(
+                title="Staff Role",
+                description=f"The staff role  for {ctx.guild.name} has been set to {role.mention}",
+                timestamp=datetime.utcnow(),
+                color=self.bot.color,
+            )
+            await ctx.send(embed=embed)
+
+    @role.group(name="list", brief="List the admin, mod and staff roles of the server")
+    @commands.has_permissions(administrator=True)
+    async def list_command(self, ctx):
+        """List the admin, mod and staff roles of the server"""
+        author = ctx.author
+        guild = ctx.guild
+        model = await ModerationRoles.get_or_none(guild_id=guild.id)
+        if model is None:
+            await ctx.send("No data found.")
+            return
+        embed = Embed(
+            color=self.bot.color,
+            description=f"**Admin Role** - <@&{model.admin_role}>\n\n**Mod Role** - <@&{model.mod_role}>\n\n**Staff Role** - <@&{model.staff_role}>",
+            timestamp=datetime.utcnow(),
+        )
+        embed.set_author(name=f"Staff Roles for {guild.name}", icon_url=guild.icon_url)
+        embed.set_footer(text=f"Invoked by {author}")
+        await ctx.send(embed=embed)
 
 
 def setup(bot: Bot) -> None:
