a7af096ce878ff4f5add70a4d0f5c83935ba026b
Nishant Sapkota <snishant306@gmail.com>
2021-08-17 20:18:31 +0545

    [Update] HotFixes

--- zorander/cogs/warnings.py
+++ zorander/cogs/warnings.py
@@ -1,4 +1,4 @@
-from logging import warn
+from typing import Optional
 import discord
 from discord.channel import TextChannel
 from discord.ext import commands
@@ -6,9 +6,10 @@ from discord import Color, Member, User, Embed
 from datetime import datetime
 from discord.ext.commands import Cog, command, Greedy
 
-from zorander.core.models import WarnModel
+from zorander.core.models import WarningsModel
 from zorander.utils.permissions import Permissions
 from zorander.cogs.mod import Mod
+from zorander.utils.time import *
 
 
 from zorander import Bot
@@ -23,11 +24,11 @@ class Warnings(Cog):
 
     async def warning_count(self, ctx: commands.Context, member: Member) -> None:
         """Function that checks for amount of warnings"""
-        warn_model = await WarnModel.filter(guild_id=ctx.guild.id, member_id=member.id)
-        if len(warn_model) in list(range(5, 8)):
-            await self.mod.mute_handler(ctx, [member], 600, "Warning Count : 5")
-        if len(warn_model) in list(range(8, 10)):
-            await self.mod.mute_handler(ctx, [member], 21600, "Warning Count : 8")
+        warn_model = await WarningsModel.filter(guild_id=ctx.guild.id, member_id=member.id)
+        if len(warn_model) in range(5, 8):
+            await self.mod.mute_handler(ctx, [member], 600, f"Warning Count : {len(warn_model)}")
+        if len(warn_model) in range(8, 10):
+            await self.mod.mute_handler(ctx, [member], 21600, f"Warning Count : {len(warn_model)}")
         if len(warn_model) >= 10:
             await self.mod.kick_handler(
                 ctx, [member], f"Warning Count : {len(warn_model)}"
@@ -44,8 +45,8 @@ class Warnings(Cog):
         log_channel = await self.permissions.log_channel_check(guild)
         for member in members:
             self.permissions.has_higher_role(author, member)
-            model = await WarnModel.create(
-                guild_id=guild.id, member_id=member.id, reason=reason
+            model = await WarningsModel.create(
+                guild_id=guild.id, member_id=member.id, reason=reason, author_name = author, date = pretty_datetime(datetime.now())
             )
             await model.save()
             embed = Embed(
@@ -70,6 +71,70 @@ class Warnings(Cog):
                 pass
 
             await self.warning_count(ctx, member)
+    
+    @commands.group(invoke_without_command =True)
+    async def warnings(self, ctx: commands.Context, member: Optional[Member]):
+        """View the warnings of the member specified."""
+        author = ctx.author
+        guild  = ctx.guild
+        await self.permissions.staff_role_check(ctx, guild)
+        member = member or author
+        warn_model = await WarningsModel.filter(guild_id=guild.id, member_id=member.id)
+        
+        warnings = "\n\n".join(
+            [
+                f"#{model.id} - `{model.date}` - Warned By **{model.author_name}**\n**Reason:**{model.reason}"
+                for (i, model) in enumerate(warn_model)
+
+            ]
+        )
+        embed = Embed(
+            color=self.bot.color,
+            timestamp=datetime.utcnow(),
+            title = f"Warnings-{member}[ID - {member.id}]",
+            description=warnings if len(warn_model) else "User hasn't been warned.",
+        )
+        embed.set_footer(text=f"Requested by {author.name} | Total Warnings: {len(warn_model)}")
+        await ctx.send(embed=embed)
+
+
+    @warnings.command(name="delete")
+    async def warnings_delete(self, ctx: commands.Context, member: Optional[Member], id: int) -> None:
+        """Delete individual warnings of a member."""
+        author = ctx.author
+        guild = ctx.guild
+        await self.permissions.mod_role_check(ctx, guild)
+        log_channel = await self.permissions.log_channel_check(guild)
+        model = await WarningsModel.get_or_none(guild_id=guild.id, id=id)
+        await model.delete()
+        await ctx.send(f"Deleted Warning #{id} of **{member}**")
+        embed = Embed(
+            color = Color.green(),
+            timestamp = datetime.utcnow(),
+            description = f":warning: Deleted Warning #{id} of \n**{member} [ID {member.id}]**"
+        )
+        embed.set_author(name=f"{author} [ID {author.id}]", icon_url= author.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+        await log_channel.send(embed=embed)
+    
+
+    @warnings.command(name="clear")
+    async def warnings_clear(self, ctx: commands.Context, member: Member) -> None:
+        """Clear the warnings of the user."""
+        author = ctx.author
+        guild = ctx.guild
+        await self.permissions.admin_role_check(ctx, guild)
+        log_channel = await self.permissions.log_channel_check(guild)
+        model = await WarningsModel.filter(guild_id=guild.id, member_id=member.id).delete()
+        await ctx.send(f"Cleared Warnings of **{member}**")
+        embed = Embed(
+            color = Color.green(),
+            timestamp = datetime.utcnow(),
+            description = f":warning: Cleared Warnings of \n**{member} [ID {member.id}]**"
+        )
+        embed.set_author(name=f"{author} [ID {author.id}]", icon_url= author.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+        await log_channel.send(embed=embed)
 
 
 def setup(bot: Bot) -> None:
