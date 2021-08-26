2c0805f9fc8698ac492fd3723da4a97acad26e27
Nishant Sapkota <snishant306@gmail.com>
2021-08-26 17:31:53 +0545

    [Update] Adds ability to turn off logging

--- zorander/cogs/logging.py
+++ zorander/cogs/logging.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+from datetime import date, datetime
+from logging import log
 from operator import add
 
 import discord
 from discord import Embed, Member, Message
+from discord.abc import GuildChannel
 from discord.colour import Color
 from discord.ext import commands
-from discord.ext.commands import Cog, command
+from discord.ext.commands import Cog
+from discord.member import VoiceState
 
 from zorander import Bot
 from zorander.utils.permissions import Permissions
@@ -18,7 +21,9 @@ class Logging(Cog):
 
     @Cog.listener()
     async def on_member_update(self, before: Member, after: Member) -> None:
-        logging_channel = await self.permissions.logging_channel_check(before.guild)
+        if not await self.bot.check_logging_disable(before.guild.id):
+            return
+        logging_channel = await self.permissions.member_logging_check(before.guild)
         if before.name != after.name:
             embed = Embed(
                 color=after.color,
@@ -120,9 +125,9 @@ class Logging(Cog):
 
     @Cog.listener()
     async def on_message_edit(self, before: Message, after: Message) -> None:
-        logging_channel = await self.permissions.logging_channel_check(
-            before.author.guild
-        )
+        if not await self.bot.check_logging_disable(before.author.guild.id):
+            return
+        logging_channel = await self.permissions.message_logs_check(before.author.guild)
         if not after.author.bot:
             if before.content != after.content:
                 embed = Embed(
@@ -131,7 +136,9 @@ class Logging(Cog):
                     timestamp=datetime.utcnow(),
                 )
 
-                embed.set_author(name=f"Message Edited")
+                embed.set_author(
+                    name=f"Message Edited", icon_url=before.author.avatar_url
+                )
 
                 fields = [
                     ("Before", before.content, False),
@@ -145,7 +152,9 @@ class Logging(Cog):
 
     @Cog.listener()
     async def on_message_delete(self, message: Message) -> None:
-        logging_channel = await self.permissions.logging_channel_check(
+        if not await self.bot.check_logging_disable(message.guild.id):
+            return
+        logging_channel = await self.permissions.message_logs_check(
             message.author.guild
         )
         if not message.author.bot:
@@ -155,9 +164,62 @@ class Logging(Cog):
                 timestamp=datetime.utcnow(),
             )
 
-            embed.set_author(name=f"Message Deleted")
+            embed.set_author(
+                name=f"Message Deleted", icon_url=message.author.avatar_url
+            )
 
             embed.add_field(name="Content", value=message.content, inline=False)
+            await logging_channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_voice_state_update(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ) -> None:
+        if not await self.bot.check_logging_disable(member.guild.id):
+            return
+        logging_channel = await self.permissions.voice_logs_check(member.guild)
+        if before.channel is None and after.channel is not None:
+            embed = Embed(
+                color=Color.green(),
+                description=f"**{member}** has joined in **{after.channel.name}**",
+                timestamp=datetime.utcnow(),
+            )
+
+            embed.set_author(name=f"Voice Logging", icon_url=member.avatar_url)
+
+            await logging_channel.send(embed=embed)
+
+        if before.channel is not None and after.channel is None:
+            embed = Embed(
+                color=Color.red(),
+                description=f"**{member}** has left **{before.channel.name}**",
+                timestamp=datetime.utcnow(),
+            )
+
+            embed.set_author(name=f"Voice Logging", icon_url=member.avatar_url)
+
+            await logging_channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_guild_channel_update(
+        self, before: GuildChannel, after: GuildChannel
+    ) -> None:
+        if not await self.bot.check_logging_disable(before.guild.id):
+            return
+        logging_channel = await self.permissions.logging_channel_check(before.guild)
+        if before.name != after.name:
+            embed = Embed(color=Color.green(), timestamp=datetime.utcnow())
+
+            embed.set_author(name="Channel Name Update")
+
+            fields = [
+                ("Name (Before)", before.name, False),
+                ("Name (After)", after.name, False),
+            ]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
             await logging_channel.send(embed=embed)
 
 
